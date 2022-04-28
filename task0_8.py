bd87e501bdb895ae4438abbef9bc0665bd5e0192
Tshegofatso Letlape <tshegofatsoletlape90@gmail.com>
2022-04-29 00:33:24 +0200

    Made changes that were required

--- task0_8.py
+++ task0_8.py
@@ -4,13 +4,13 @@ def convert(number):
     hour = (number // 60)
     minutes = (number % 60)
     
-    if hour <= 1 and minutes <=1:
+    if hour == 1 and minutes ==1:
         print(f"{hour} hour, {minutes} minute")
-    elif hour >= 2 and minutes >= 2:
+    elif (hour >= 2 or hour == 0) and (minutes >= 2 or minutes == 0):
         print(f"{hour} hours, {minutes} minutes")
     elif hour <= 1 and minutes >=2:
         print(f"{hour} hour, {minutes} minutes")
     else:
         print(f"{hour} hours, {minutes} minute")
   
-convert(0)
+convert(188)
