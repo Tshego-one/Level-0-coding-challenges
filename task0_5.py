a9159386c3468d8cfb6f5362da50d51c018e4fa1
Tshegofatso Letlape <tshegofatsoletlape90@gmail.com>
2022-05-04 07:08:02 +0200

    Made the changes that were asked for

--- task0_5.py
+++ task0_5.py
@@ -1,6 +1,6 @@
 def area_of_triangle(side1, side2, side3):
 
-    s = (side1 + side2 + side3) / 2
-    area = ((s*(s-side1)*(s-side2)*(s-side3)))**(1/2)
+    semi_perimeter = (side1 + side2 + side3) / 2
+    area = ((semi_perimeter*(semi_perimeter - side1)*(semi_perimeter - side2)*(semi_perimeter - side3)))**(1/2)
     return area
 print(area_of_triangle(2, 3, 4))
